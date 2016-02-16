014a1dc5825dfa4acb1f077ebe1e28d7682de94e
Andrew Swenson <swena56@gmail.com>
2016-02-15 20:29:29 -0600

    p2p and sockets training

--- automatedInstallOnWindows.bat
+++ automatedInstallOnWindows.bat
@@ -1,10 +1,11 @@
 
 REM download python27
-powershell Invoke-WebRequest https://www.python.org/ftp/python/2.7.11/python-2.7.11.amd64.msi -OutFile %TEMP%\python-2.7.11.amd64.msi
+REM powershell Invoke-WebRequest https://www.python.org/ftp/python/2.7.11/python-2.7.11.amd64.msi -OutFile %TEMP%\python-2.7.11.amd64.msi
+powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://www.python.org/ftp/python/2.7.11/python-2.7.11.amd64.msi')"
 
 REM install python27
 REM /a for admininistrative install
-msiexec /i %TEMP%\python-2.7.11.amd64.msi TARGETDIR=%TEMP%\python27\ /quiet
+msiexec /a /i python-2.7.11.amd64.msi TARGETDIR=%TEMP%\python27\ /quiet
 
 REM remove install file
 setx path "%path%;%TEMP%\python27\
@@ -12,5 +13,9 @@ setx path "%path%;%TEMP%\python27\
 REM add to path
 
  
+
+
+
+
 
 
---
+++ menu.py
@@ -0,0 +1,5 @@
+#template menu
+print "\nTitle"
+print "1) "
+print "2) "
+raw_input("make a selection: ")
