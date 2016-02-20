fdfef7b5cb98072ca363f47898f0aa7558b6678f
Andrew Swenson <swena56@gmail.com>
2016-02-19 22:45:05 -0500

    asynchronous server and send to server

---
+++ p2p/sendToServer.py
@@ -0,0 +1,15 @@
+import socket
+
+def client(ip, port, message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ip, port))
+    try:
+        sock.sendall(message)
+        response = sock.recv(1024)
+        print "Received: {}".format(response)
+    finally:
+        sock.close()
+
+
+client('hostaddr',62,"hi server")
+
