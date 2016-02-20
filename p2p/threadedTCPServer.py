e6b18b3d6734d5224f26f1155920884c8861ec30
Andrew Swenson <swena56@gmail.com>
2016-02-19 20:35:10 -0800

    added connect to client option

--- p2p/threadedTCPServer.py
+++ p2p/threadedTCPServer.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import SocketServer
+from btpeer import *
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 
@@ -16,6 +17,8 @@ class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
 def client(ip, port, message):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ip, port))
+    conn = BTPeerConnection(2,ip,port,sock, True)
+    print vars(conn)
     try:
         sock.sendall(message)
         response = sock.recv(1024)
@@ -25,7 +28,7 @@ def client(ip, port, message):
 
 if __name__ == "__main__":
     # Port 0 means to select an arbitrary unused port
-    HOST, PORT = "0.0.0.0", 62
+    HOST, PORT = "0.0.0.0", 5669
 
     server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
     ip, port = server.server_address
@@ -41,11 +44,35 @@ if __name__ == "__main__":
     #    client(ip, port, "Hello World 1")
     #    client(ip, port, "Hello World 2")
     #    client(ip, port, "Hello World 3")
+
+    
+    a = BTPeer(5,5670,None,'swena56.ddns.net')
+    b = BTPeer(5,5670,None,'docker.me')
+  
+    
+    #define peers
     running = True
 
-     #main loop
+    #main loop
     while running == True:
-	if running == False: break
+	#os.system('cls')       
+        print "\n\n------------------Menu-------------------"
+        print "Listening socket: "
+        print "-----------------------------------------"
+        print "1)Connect to hose"
+        print "2)Show Peer List"
+        print "X)"
+        print "9)Quit"
+        print "\n"
+        input = raw_input("selection:")
+        if input is "1":
+                connectTo = raw_input("connect what host: ")
+                if True:
+                    client(connectTo,port,"message")
+        if input is "2":
+                showPeers()
+        if input is "9":
+                running = False
 
     server.shutdown()
     server.server_close()
