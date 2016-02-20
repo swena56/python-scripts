dd6a3de162787f97319719d29a8a4db873099b1e
Andrew Swenson <swena56@gmail.com>
2016-02-19 19:06:16 -0500

    server and client for sending a message

---
+++ p2p/server.py
@@ -0,0 +1,27 @@
+import socket
+import urllib #needed for other features which i didn't include here for brievity
+
+s = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
+s.bind ( ( '', 8642 ) )
+
+s.listen ( 1 )
+while True:
+   channel, details = s.accept()
+   while(1):
+       data= channel.recv ( 100 )
+       command=data.split(" ")
+       if not data:
+           break
+       else:
+           if command[0] == "/terminate":
+               channel.send("Client exit")
+               channel.close()
+               s.close()
+               exit(1);
+           elif command[0] == "/quit":
+               channel.send("Client exit")
+           else:
+               print data
+               str="received: "+data
+               channel.send(str)
+   channel.close()
